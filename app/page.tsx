2ca3b5ef5846f47f73dae41a84ada6d7b6a6e680
Ranju Shrestha <ranjustha8844@gmail.com>
2026-04-24 14:04:13 +0545

    Initial project setup (Next.js + Tailwind + shadcn)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
   return (
-   <main><h1 className="text-red-500">Home Page</h1></main>
+    <div className="p-10 max-w-xl mx-auto">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            Click to open
+          </AccordionTrigger>
+
+          <AccordionContent>
+            This content should animate smoothly 👇
+            <div className="mt-2 bg-blue-500 text-white p-3 rounded">
+              Animation Test Content
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   );
 }
